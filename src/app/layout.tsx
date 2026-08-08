32021e73984a0f6ad9cc833360e71bf9908c6008
Agus Hidayatullah <agushidayatullah@MacBook-Air-Agus.local>
2026-08-09 04:27:20 +0700

    fix: move GTM script to first position in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,15 +90,15 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="id" className={`${oswald.variable} ${barlow.variable}`}>
       <head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-W7S5MNDD');` }} />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
       </head>
       <body className="min-h-screen flex flex-col">
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W7S5MNDD"
